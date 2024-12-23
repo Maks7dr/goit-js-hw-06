dab490258af447f70086f05620f35f5c579446ae
Maksym Drachenko <maks7d@gmail.com>
2024-12-23 20:03:45 +0500

    add task-3

---
+++ js/task-3.js
@@ -0,0 +1,27 @@
+class StringBuilder {
+  #value = {
+    initialValue,
+  };
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value.push(str);
+  }
+  padStart(str) {
+    this.#value.unshift(str);
+  }
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+  }
+}
+
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
