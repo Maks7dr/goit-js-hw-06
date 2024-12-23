e1fcc1c493c52c933ee4a6ef6800b12f9d2b8933
Maksym Drachenko <maks7d@gmail.com>
2024-12-23 19:36:56 +0500

    add task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,34 +1,30 @@
-class Car {
-  #brand;
-  #model;
-  #price;
-  constructor(params) {
-    this.#brand = params.brand;
-    this.#model = params.model;
-    this.#price = params.price;
-  }
+const customer = {
+  username: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['Burger', 'Pizza', 'Salad'],
+  // Change code below this line
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+  // Change code above this line
+};
 
-  get brand() {
-    return this.#brand;
-  }
-
-  set brand(newBrand) {
-    this.#brand = newBrand;
-  }
-
-  get model() {
-    return this.#model;
-  }
-
-  set model(newModel) {
-    this.#model = newModel;
-  }
-
-  get price() {
-    return this.#price;
-  }
-
-  set price(newPrice) {
-    this.#price = newPrice;
-  }
-}
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, 'Steak');
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
